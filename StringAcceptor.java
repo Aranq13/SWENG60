f510757696b7607e438cf908e8c65c7780066d36
Aranq13 <98594133+Aranq13@users.noreply.github.com>
2022-10-05 04:57:40 +0100

    Update StringAcceptor.java
    Update function, now only needs to accept negative numbers

--- StringAcceptor.java
+++ StringAcceptor.java
@@ -1,26 +1,24 @@
-    boolean stringAcceptor(String input) {
+       boolean stringAcceptor(String input) {
 
         if ((input.charAt(0) < '0') | (input.charAt(0) > '9')) {
             return false;
         }
 
-        if ((input.charAt(input.length()) < '0') | (input.charAt(input.length()) > '9')) {
+        if ((input.charAt(input.length()-1) < '0') | (input.charAt(input.length()-1) > '9')) {
             return false;
         }
 
 
         boolean prevOperator = false;
-        for (int i = 1; i <= input.length(); i++) {
+        for (int i = 1; i < input.length(); i++) {
             char next = input.charAt(i);
             if (next == '+' || next == '-' || next == '*') {
                 if (prevOperator) return false;
                 else prevOperator = true;
             }
-            else if ( next > '9') {
+            else if ( next > '9' || next < '0') {
                 return false;
-            }
-
-
+            } else prevOperator = false;
         }
 
         return true;
