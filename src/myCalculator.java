79b95a877db5f5312a2df4d5bc12ef21c0c67718
emukperv <emukperv@tcd.ie>
2022-10-08 16:49:35 +0100

    implemented use of stacks and precedence

--- src/myCalculator.java
+++ src/myCalculator.java
@@ -1,24 +1,68 @@
+package src;
+
 // imports
 
 import java.util.*;
 
 public class myCalculator {
-    public static int parse(String input) {
+
+    // return true if 2nd operator has higher or equal precendence to 1st operator
+    // otherwise, returns false
+    public static boolean hasPrecedence(char operator1, char operator2) {
+        if (operator2 == '(' || operator2 == ')') return false;
+        if ((operator1 == '*') && (operator2 == '+' || operator2 == '-')) return false;
+        else return true;
+    }
+
+    public static int applyOperator(char operator, int x, int y) {
+        switch(operator) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y; 
+        }
+        return 0;
+    }
+
+    public static int evaluate(String input) {
         input.replaceAll("\\s+",""); // remove whitespace (if any)
-        String operators[] = input.split("[0-9]+"); // find character literals 0-9, match more than one times
-        String operands[]= input.split("(?<=\\d)[+*-]"); // +, - or * is followed by digit
-        int eq = Integer.parseInt(operands[0]); // turn string numbers into integer
-        for (int i = 1; i < operands.length; i++) { // recuris
-            if(operators[i].equals("+")) {
-                eq += Integer.parseInt(operands[i]); 
-            } else if(operators[i].equals("-")) {
-                eq += Integer.parseInt(operands[i]);
-            } else {
-                eq *= Integer.parseInt(operands[i]);
+        char[] element = input.toCharArray(); // add individual char to char array
+        Stack<Integer> operand = new Stack<Integer>(); // stack for operands
+        Stack<Character> operator = new Stack<Character>(); // stack for operators
+        for (int i = 0; i < element.length; i++) {
+            if (element[i] >= '0' && element[i] <= '9') { // if number, push to operand stack
+                StringBuffer elementbuf = new StringBuffer(); // modifiable strings
+                while (i < element.length && element[i] >= '0' && element[i] <= '9') { // x>1 digits
+                    elementbuf.append(element[i++]); // add extra digit to number
+                    operand.push(Integer.parseInt(elementbuf.toString())); //push number to stack
+                }
+                i--; // correct offset (i points to next char, for loop decreases i)
+            }
+            else if (element[i] == '(') {
+                operator.push(element[i]); // opening bracket pushes to op
+            } else if (element[i] == ')') {  // closing bracket, solves equation in bracket
+                while (operator.peek() != '(') {
+                    operand.push(applyOperator(operator.pop(), operand.pop(), operand.pop()));
+                } 
+                operator.pop();
+            } else if (element[i] == '+' || element[i] == '-' || element[i] == '*') { // current i is an operator
+                // while top of operator stack has = or > precedence to current i
+                // apply operator on top of top two elements in operand stack
+                while (!operator.empty() && hasPrecedence(element[i], operator.peek())) { 
+                    operand.push(applyOperator(operator.pop(), operand.pop(), operand.pop()));
+                }
+                operator.push(element[i]); // push current element to operator stack
             }
         }
-        return eq;
+        while (!operator.empty()) { // apply remaining operators to remaining values
+            operand.push(applyOperator(operator.pop(), operand.pop(), operand.pop()));
+        }
+        return operand.pop(); // return answer on top of stack
     }
+
+
     public static void main (String[] args) {
             try (Scanner input = new Scanner(System.in)) {
                 System.out.println("Welcome to The String Calculator!\nPlease type exit when ready to exit program.\n\n");
@@ -28,7 +72,7 @@ public class myCalculator {
                     if ("exit".equalsIgnoreCase(equation)) {
                         break;
                     }
-                    int answer = parse(equation);
+                    int answer = evaluate(equation);
                 System.out.println("Answer: " + answer);
                 }
             } catch(java.lang.Exception e) {e.printStackTrace();}
