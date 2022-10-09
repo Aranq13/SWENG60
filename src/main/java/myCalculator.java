eae345b882560b7ac68870dea1eadedf4cb529e1
Aranq <aranq13@gmail.com>
2022-10-09 03:57:45 +0100

    Update unit tests and fix negative numbers

--- src/main/java/myCalculator.java
+++ src/main/java/myCalculator.java
@@ -1,25 +1,22 @@
-// imports
-
 import java.util.*;
 
 public class myCalculator {
-    public static int evaluate(String input) {
-        input.replaceAll("\\s+",""); // remove whitespace (if any)
-        String operators[] = input.split("[0-9]+"); // find character literals 0-9, match more than one times
-        String operands[]= input.split("(?<=\\d)[+*-]"); // +, - or * is followed by digit
-        int eq = Integer.parseInt(operands[0]); // turn string numbers into integer
-        for (int i = 1; i < operands.length; i++) { // recuris
-            if(operators[i].equals("+")) {
-                eq += Integer.parseInt(operands[i]); 
-            } else if(operators[i].equals("-")) {
-                eq += Integer.parseInt(operands[i]);
-            } else {
-                eq *= Integer.parseInt(operands[i]);
-            }
+
+    // return true if 2nd operator has higher or equal precedence to 1st operator
+    // otherwise, returns false
+    public static int precedence(char o) {
+        switch (o) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+                return 2;
+            case '(':
+            case ')':
+                return 3;
         }
-        return eq;
+        return -1;
     }
-    
     public static boolean stringAcceptor(String input) {
         if (input == null || input.length() == 0) return false;
         int first = 0;
@@ -48,18 +45,117 @@ public class myCalculator {
         return true;
     }
 
-    public static void main (String[] args) {
-            try (Scanner input = new Scanner(System.in)) {
-                System.out.println("Welcome to The String Calculator!\nPlease type exit when ready to exit program.\n\n");
-                System.out.println("Please enter your equation: ");
-                while (true) {
-                    String equation = input.nextLine();
-                    if ("exit".equalsIgnoreCase(equation)) {
+    // negative numbers e.g -30 + 3, bugged as + is being popped first
+    public static int applyOperator(Stack<Integer> operand, Stack<Character> operators) {
+        int x, y;
+        // if(operators.size() >= 2) {
+        char operator = operators.pop();
+        switch(operator) {
+            case '+':
+                x = operand.pop();
+                y = operand.pop();
+                return x + y;
+            case '-':
+                if(operand.size() == 1) {
+                    x = operand.pop();
+                    return -x;
+                } else
+                    y = operand.pop();
+                x = operand.pop();
+                return x - y;
+            case '*':
+                x = operand.pop();
+                y = operand.pop();
+                return x * y;
+        }
+        return 0;
+    }
+
+    public static boolean isOperator(char o) {
+        return (o == '+' || o == '-' || o == '*');
+    }
+
+    public static int evaluate(String input) {
+        boolean isNegative = input.charAt(0) == '-';
+        boolean prevOperator = true;
+        input = input.replaceAll("\\s+",""); // remove whitespace (if any)
+        Stack<Integer> operand = new Stack<Integer>(); // stack for operands
+        Stack<Character> operator = new Stack<Character>(); // stack for operators
+        for (int i = 0; i < input.length(); i++) {
+            char k = input.charAt(i); // element position
+            if (Character.isDigit(k)) { // if operand...
+                int number = 0;
+                while (Character.isDigit(k)) {
+                    number = number*10 + (k - '0'); // convert to integer
+                    i++;
+                    if (i < input.length()) { // if more than one digit in number
+                        k = input.charAt(i);
+                    } else {
                         break;
                     }
-                    int answer = evaluate(equation);
-                System.out.println("Answer: " + answer);
                 }
-            } catch(java.lang.Exception e) {e.printStackTrace();}
+                i--;
+                if (isNegative){
+                    number = -number;
+                    isNegative = false;
+                }
+                operand.push(number);	// push numbers onto operand stack
+                prevOperator = false;
+            } else if (k == '(') {	// push opening bracket to operator stack
+                operator.push(k);
+                prevOperator=true;
+            } else if (k == ')') { 	// closing bracket solves equation
+                while(operator.peek()!='(') {
+                    int answer = applyOperator(operand, operator);
+                    operand.push(answer);
+                }
+                operator.pop();
+            } else if (isOperator(k)) {	// if current k is an operator
+                if (i == 0 || operand.size() == 1) {
+                    if (prevOperator & k == '-'){
+                        isNegative = true;
+                    } else {
+                        operator.push(k);
+                        prevOperator = true;
+                    }
+                } else {
+                    while(operand.size() >= 2 && precedence(k) <= precedence(operator.peek())) { // if precedence is higher...
+                        int out = applyOperator(operand, operator);
+                        operand.push(out);
+                    }
+
+                }
+            }
+            while (!operator.isEmpty() && !operand.isEmpty() && operand.size() >= 2) { // apply remaining operators to remaining numbers
+                int out = 0;
+                if (operand.size() >= 2 && operator.size() != 0) {
+                    for( i = 0; i < operand.size(); i++) {
+                        out = operand.push(applyOperator(operand, operator));
+                    }
+                    if(operator.size() != 0)
+                        out = applyOperator(operand, operator);
+                }
+                operand.push(out);
+                return operand.pop();
             }
         }
+        return operand.pop();	// return answer on top of stack
+    }
+
+
+
+    public static void main (String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Welcome to The String Calculator!\nPlease type exit when ready to exit program.\n\n");
+            System.out.println("Please enter your equation: ");
+            while (true) {
+                String equation = input.nextLine();
+                if ("exit".equalsIgnoreCase(equation)) {
+                    break;
+                }
+                int answer = evaluate(equation);
+                System.out.println("Answer: " + answer);
+            }
+        } catch(java.lang.Exception e) {e.printStackTrace();}
+    }
+}
--- src/test/java/UnitTests.java
+++ src/test/java/UnitTests.java
@@ -9,76 +9,97 @@ public class UnitTests {
 
 //~ Constructor ........................................................
 
-    
+    @Test
     /**
      * Tests that valid strings are accepted and invalid strings are rejected.
      * stringAcceptor not yet implemented on main branch
+     */
      
     public void acceptorTest() {
-        myCalculator calculator = new myCalculator();
-
-        assertFalse(calculator.stringAcceptor(null));
-        assertFalse(calculator.stringAcceptor(""));
-        assertFalse(calculator.stringAcceptor("hello"));
-        assertFalse(calculator.stringAcceptor("x+y"));
-        assertFalse(calculator.stringAcceptor("3x+2y-5"));
-        assertFalse(calculator.stringAcceptor("hello"));
-        assertFalse(calculator.stringAcceptor("+30"));
-        assertFalse(calculator.stringAcceptor("*30"));
-        assertTrue(calculator.stringAcceptor("-30"));
-        assertTrue(calculator.stringAcceptor("-30+55"));
-        assertTrue(calculator.stringAcceptor("-30+55*26+54"));
-        assertFalse(calculator.stringAcceptor("-30+55-*26+54"));
-        assertTrue(calculator.stringAcceptor("-30+55*-26+54"));
-        assertFalse(calculator.stringAcceptor("-30+55*26+54-"));
+        assertFalse(myCalculator.stringAcceptor(null));
+        assertFalse(myCalculator.stringAcceptor(""));
+        assertFalse(myCalculator.stringAcceptor("hello"));
+        assertFalse(myCalculator.stringAcceptor("x+y"));
+        assertFalse(myCalculator.stringAcceptor("3x+2y-5"));
+        assertFalse(myCalculator.stringAcceptor("hello"));
+        assertFalse(myCalculator.stringAcceptor("+30"));
+        assertFalse(myCalculator.stringAcceptor("*30"));
+        assertTrue(myCalculator.stringAcceptor("-30"));
+        assertTrue(myCalculator.stringAcceptor("-30+55"));
+        assertTrue(myCalculator.stringAcceptor("-30+55*26+54"));
+        assertFalse(myCalculator.stringAcceptor("-30+55-*26+54"));
+        assertTrue(myCalculator.stringAcceptor("-30+55*-26+54"));
+        assertFalse(myCalculator.stringAcceptor("-30+55*26+54-"));
     }
-    */
-    
+
     @Test
     /**
-     * Tests that the evaluate method will return the correct result for equations with a single operator
-     * Subtraction not working on main branch
+     * Tests that the evaluate method will return the correct result when adding numbers
      */
-    public void singleTest() {
-        myCalculator calculator = new myCalculator();
-
-        assertEquals(0, calculator.evaluate("0"));
-        assertEquals(55, calculator.evaluate("5+50"));
-        //assertEquals(23, calculator.evaluate("30-7"));
-        assertEquals(39, calculator.evaluate("13*3"));
+    public void additionTest() {
+        assertEquals(0, myCalculator.evaluate("0"));
+        assertEquals(5, myCalculator.evaluate("2+3"));
+        assertEquals(18, myCalculator.evaluate("9+9"));
+        assertEquals(27, myCalculator.evaluate("9+9+9"));
+        assertEquals(107, myCalculator.evaluate("100+7"));
+        assertEquals(77, myCalculator.evaluate("7+70"));
+        assertEquals(82, myCalculator.evaluate("7+70+5"));
+        assertEquals(162, myCalculator.evaluate("110+50+2"));
     }
 
-    
+    @Test
+    /**
+     * Tests that the evaluate method will return the correct result when subtracting numbers
+     */
+    public void subtractionTest() {
+        assertEquals(4, myCalculator.evaluate("9-5"));
+        assertEquals(20, myCalculator.evaluate("30-10"));
+        assertEquals(23, myCalculator.evaluate("30-7"));
+        assertEquals(39, myCalculator.evaluate("100-61"));
+    }
+
+    @Test
+    /**
+     * Tests that the evaluate method will return the correct result when multiplying numbers
+     */
+    public void multiplicationTest() {
+        assertEquals(0, myCalculator.evaluate("1*0"));
+        assertEquals(0, myCalculator.evaluate("0*1"));
+        assertEquals(1, myCalculator.evaluate("1*1"));
+        assertEquals(9, myCalculator.evaluate("3*3"));
+        assertEquals(81, myCalculator.evaluate("9*9"));
+        assertEquals(39, myCalculator.evaluate("3*13"));
+        assertEquals(39, myCalculator.evaluate("13*3"));
+        assertEquals(605, myCalculator.evaluate("121*5"));
+    }
+
+    @Test
     /**
      * Tests that the evaluate method will return the correct result for equations featuring negative numbers
      * Failure on main branch (bugged)
      */
     public void negativeNumbersTest() {
-        myCalculator calculator = new myCalculator();
-
-        assertEquals(-5, calculator.evaluate("-5"));
-        assertEquals(40, calculator.evaluate("50+-10"));
-        assertEquals(-27, calculator.evaluate("-30+3"));
-        assertEquals(12, calculator.evaluate("6--6"));
-        assertEquals(0, calculator.evaluate("-6--6"));
-        assertEquals(-35, calculator.evaluate("5*-7"));
+        assertEquals(-5, myCalculator.evaluate("-5"));
+        assertEquals(40, myCalculator.evaluate("50+-10"));
+        assertEquals(-27, myCalculator.evaluate("-30+3"));
+        assertEquals(12, myCalculator.evaluate("6--6"));
+        assertEquals(0, myCalculator.evaluate("-6--6"));
+        assertEquals(-35, myCalculator.evaluate("5*-7"));
     }
 
-    
+    @Test
     /**
      * Tests that the calculator will give the correct results for equations with order of operations
      * Failure on main branch (oop not yet implemented)
      */
     public void oopTest() {
-        myCalculator calculator = new myCalculator();
-
-        assertEquals(15, calculator.evaluate("5+5+5"));
-        assertEquals(19, calculator.evaluate("10+4*2+1"));
-        assertEquals(-50, calculator.evaluate("5-5--5*10"));
-        assertEquals(67, calculator.evaluate("85-9*2"));
-        assertEquals(30, calculator.evaluate("3*3*3+3"));
-        assertEquals(72, calculator.evaluate("-24--24--24--24"));
-        assertEquals(72, calculator.evaluate("-24--24*3"));
+        assertEquals(15, myCalculator.evaluate("5+5+5"));
+        assertEquals(19, myCalculator.evaluate("10+4*2+1"));
+        assertEquals(-50, myCalculator.evaluate("5-5--5*10"));
+        assertEquals(67, myCalculator.evaluate("85-9*2"));
+        assertEquals(30, myCalculator.evaluate("3*3*3+3"));
+        assertEquals(72, myCalculator.evaluate("-24--24--24--24"));
+        assertEquals(72, myCalculator.evaluate("-24--24*3"));
     }
 
 }
